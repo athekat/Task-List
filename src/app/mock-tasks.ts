efc059f037871d5a92edff9c9b45aba01aa236dd
alanthekat <alanc90@gmail.com>
2022-02-20 16:57:48 -0300

    agrega mock de tareas e interface para las mismas

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id:1,
+        text:"Terminar primer modulo de Angular",
+        day: 'Agosto 5 a las 12:00hs',
+        reminder: true
+    },
+    {
+        id:2,
+        text:"Terminar segundo modulo de Angular",
+        day: 'Agosto 18 a las 12:00hs',
+        reminder: true
+    },
+    {
+        id:3,
+        text:"Terminar tercer modulo de Angular",
+        day: 'Agosto 29 a las 12:00hs',
+        reminder: true
+    }
+]
